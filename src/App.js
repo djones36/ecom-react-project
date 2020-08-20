fd5e1f81545fcea2605d903fb1b772fa4b35d5cc
djones36 <daniel.jones2000@gmail.com>
2020-08-20 15:00:40 -0700

    building layout

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import "./App.css";
 import Home from "./components/home/home";
 import About from "./components/about/about";
+import Nav from "./components/home/nav";
 function App() {
   return (
     <div className="App">
       <Home />
+      <Nav />
       <About />
     </div>
   );
--- src/components/about/about.js
+++ src/components/about/about.js
@@ -2,29 +2,25 @@ import React from "react";
 import { Grid, Image } from "semantic-ui-react";
 
 const About = () => (
-  <Grid columns={3} divided>
+  <Grid celled>
     <Grid.Row>
-      <Grid.Column>
-        <Image src="/images/wireframe/media-paragraph.png" />
-        <p>test</p>
+      <Grid.Column width={3}>
+        <Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
       </Grid.Column>
-      <Grid.Column>
-        <Image src="/images/wireframe/media-paragraph.png" />
-      </Grid.Column>
-      <Grid.Column>
-        <Image src="/images/wireframe/media-paragraph.png" />
+      <Grid.Column width={13}>
+        <Image src="https://react.semantic-ui.com/images/wireframe/centered-paragraph.png" />
       </Grid.Column>
     </Grid.Row>
 
     <Grid.Row>
-      <Grid.Column>
-        <Image src="/images/wireframe/media-paragraph.png" />
+      <Grid.Column width={3}>
+        <Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
       </Grid.Column>
-      <Grid.Column>
-        <Image src="/images/wireframe/media-paragraph.png" />
+      <Grid.Column width={10}>
+        <Image src="https://react.semantic-ui.com/images/wireframe/paragraph.png" />
       </Grid.Column>
-      <Grid.Column>
-        <Image src="/images/wireframe/media-paragraph.png" />
+      <Grid.Column width={3}>
+        <Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
       </Grid.Column>
     </Grid.Row>
   </Grid>
