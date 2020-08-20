0627cfeb7021061b7bccfd792640e01ee72e66ae
djones36 <daniel.jones2000@gmail.com>
2020-08-20 13:02:55 -0700

    testing render of text

---
+++ src/components/about/about.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Grid, Image } from "semantic-ui-react";
+
+const About = () => (
+  <Grid columns={3} divided>
+    <Grid.Row>
+      <Grid.Column>
+        <Image src="/images/wireframe/media-paragraph.png" />
+      </Grid.Column>
+      <Grid.Column>
+        <Image src="/images/wireframe/media-paragraph.png" />
+      </Grid.Column>
+      <Grid.Column>
+        <Image src="/images/wireframe/media-paragraph.png" />
+      </Grid.Column>
+    </Grid.Row>
+
+    <Grid.Row>
+      <Grid.Column>
+        <Image src="/images/wireframe/media-paragraph.png" />
+      </Grid.Column>
+      <Grid.Column>
+        <Image src="/images/wireframe/media-paragraph.png" />
+      </Grid.Column>
+      <Grid.Column>
+        <Image src="/images/wireframe/media-paragraph.png" />
+      </Grid.Column>
+    </Grid.Row>
+  </Grid>
+);
+
+export default About;
---
+++ src/components/home/home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>MASTER PHOTOGRAPHER</h1>
+      <h2>Miss. Kaily B.</h2>
+    </div>
+  );
+};
+export default Home;
